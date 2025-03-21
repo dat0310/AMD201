f7b83c0e999d289fd9c2aa1322a241dcce9e4852
dat0310 <datpro0523@gmail.com>
2025-03-21 16:21:28 +0700

    Your commit message for submodule changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# AMD201" 
+"# UrlShortener" 
--- obj/Debug/net6.0/UrlShortener.AssemblyInfo.cs
+++ obj/Debug/net6.0/UrlShortener.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425feb11984540f40b7037b18cddbb30bb44ad9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
